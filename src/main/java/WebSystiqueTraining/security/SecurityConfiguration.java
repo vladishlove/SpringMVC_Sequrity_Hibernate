6ce80bca60daab54c72fdd72d7a5587dbdc54408
vneplokh <neplokh.v@gmail.com>
2016-12-13 11:18:53 +0200

    security

--- src/main/java/WebSystiqueTraining/security/SecurityConfiguration.java
+++ src/main/java/WebSystiqueTraining/security/SecurityConfiguration.java
@@ -1,4 +1,64 @@
 package WebSystiqueTraining.security;
 
-public class SecurityConfiguration {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+	@Autowired
+	@Qualifier("customUserDetailService")
+	UserDetailsService userDetailsService;
+
+	@Autowired
+	PersistentTokenRepository tokenRepository;
+
+	@Autowired
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+		auth.authenticationProvider(authenticationProvider());
+
+	}
+
+	@Override
+	public void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.authorizeRequests().antMatchers("/", "/list")
+				.access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
+				.antMatchers("/newUser/**", "delete-user-*").access("hasRole('ADMIN')")
+				.antMatchers("/edit-user-*").access("hasRole('ADMIN') or hasRole('DBA')")
+				.and().formLogin().loginPage("/login").loginProcessingUrl("/login").usernameParameter("ssoId")
+				.passwordParameter("password").and().rememberMe().rememberMeParameter("remember-me")
+				.tokenRepository(tokenRepository).tokenValiditySeconds(86400).and()
+				.csrf().and().exceptionHandling().accessDeniedPage("/accessDenied");
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
+	}
+
+	private AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		authenticationProvider.setUserDetailsService(userDetailsService);
+
+		return authenticationProvider;
+	}
 }
